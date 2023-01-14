7b051ad008eff4f991818e9f9af5dd68a8214927
Piotr Patrzyk <piotr@patrzyk.me>
2023-01-14 18:58:07 +0100

    fix getting repo directory

--- example_setup.sh
+++ example_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script sets variables if it's run as an example
-export GITBI_REPO_DIR=$( dirname -- "$0" | xargs realpath)
+# https://stackoverflow.com/a/246128
+export GITBI_REPO_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export GITBI_POKEMON_TYPE=sqlite
 export GITBI_POKEMON_CONN="$GITBI_REPO_DIR/pokemon.sqlite"
