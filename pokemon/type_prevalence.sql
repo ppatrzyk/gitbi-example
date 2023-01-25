6a0ae92bb09f968266c6904ed3d725ee5d59d4c7
Piotr Patrzyk <piotr@patrzyk.me>
2023-01-25 20:03:59 +0100

    sample vega viz

--- pokemon/type_prevalence.sql
+++ pokemon/type_prevalence.sql
@@ -2,9 +2,7 @@
 select
     type1,
     type2,
-    count(*) as count,
-    sum(count(*)) OVER (PARTITION BY type1) as type1_total,
-    sum(count(*)) OVER (PARTITION BY type2) as type2_total
+    count(*) as count
 from pokemons
 group by type1, type2
 order by type1, type2 desc;
